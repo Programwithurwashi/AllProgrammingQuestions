f2d38f374d109d13f14b2e8af41a963f128a425e
Programwithurwashi <86796893+Programwithurwashi@users.noreply.github.com>
2022-03-05 08:28:26 -0800

    check even or odd

---
+++ check even or odd.py
@@ -0,0 +1,6 @@
+#WAP to print the given number is even or odd
+n=int(input("Enter the number to check that it is even or odd"))
+if n%2==0:
+    print("\nGiven number is even")
+else:
+    print("\nGiven number is odd")
