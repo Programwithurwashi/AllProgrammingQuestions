0d4aa31391a94742d044be7485ccef950110f0f5
Programwithurwashi <86796893+Programwithurwashi@users.noreply.github.com>
2022-03-05 09:42:42 -0800

    number duvisible by 5

---
+++ number divisible by 5.py
@@ -0,0 +1,6 @@
+#WAP to find whether a number is divisible by 5
+n=int(input("Enter the number"))
+if n%5==0:
+    print("\n Entered number is divisible by 5")
+else:
+    print("\n Entered number is not divisible by 5")
