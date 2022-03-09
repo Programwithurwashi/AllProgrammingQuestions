11454b1b6d3761fd2306365ec5cb60955986afd7
Programwithurwashi <86796893+Programwithurwashi@users.noreply.github.com>
2022-03-09 08:47:05 -0800

    1 to 7 patten print

---
+++ 1 to 7 pattern print .py
@@ -0,0 +1,17 @@
+#WAP to print this pattern
+#1
+#22
+#333
+#4444
+#55555
+#666666
+#7777777
+
+for i in range(1,8):
+    for j in range(i):
+        print(i,end=' ')
+    print('')
+        
+        
+        
+        
