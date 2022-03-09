3ef8a2c40e863d587227947961f85f46d73946b4
Programwithurwashi <86796893+Programwithurwashi@users.noreply.github.com>
2022-03-09 04:30:20 -0800

    check array is repeating or not

---
+++ check array is repeating or not.py
@@ -0,0 +1,22 @@
+# WAP to tell whether an array contains distinct elements or not.//not repeating element 
+lst=[4,3,5,6,3,8,9,4,4,4,4]
+for i in range(0,10):
+    for j in range(i+1,11):
+        if lst[i]==lst[j]:
+            print("list has repeating element",lst[i])
+         
+
+    
+
+    
+    
+     
+    
+    
+    
+    
+    
+    
+    
+    
+     
