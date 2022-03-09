59ba0d8fad762a51bc34ce56e1b244291c37041b
Programwithurwashi <86796893+Programwithurwashi@users.noreply.github.com>
2022-03-08 21:25:55 -0800

    no of days in the month with user input

---
+++ no of days in the month with user input.py
@@ -0,0 +1,30 @@
+#WAP to tell how many days are there in a given month (month is provide by user as input)
+month=input("enter the month  ")
+if month=='january':
+    print("January has 31 days")
+elif month=='feburary':
+    print("feburary has 28 days")
+elif month=='march':
+    print("mARCH has 31 days")
+elif month=='april':
+    print("April has 30 days")
+elif month=='may':
+    print("MAy has 31 days") 
+elif month=='June':
+    print("june has 30 days")
+elif month=='july':
+    print("July has 31 days")
+elif month=='august':
+    print("August has 31 days")
+elif month=='September':
+    print("September has 30 days")
+elif month=='October':
+    print("0ctober has 31 days")
+elif month=='november':
+    print("November has 30 days")
+elif month=='december':
+    print("December has 31 days")
+else:
+    print("wrong input")
+    
+    
