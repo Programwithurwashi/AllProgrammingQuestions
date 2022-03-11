f224cb293c53f08b9cf4904481a7481477e4cf50
Programwithurwashi <86796893+Programwithurwashi@users.noreply.github.com>
2022-03-11 06:16:45 -0800

    swap two numbers using temp

---
+++ reverse of array.py
@@ -0,0 +1,5 @@
+#WAP to write reverse of array
+import array as arr
+arr=[1,2,3,4]
+rev=arr[::-1]
+print(rev)
---
+++ swap two numbers using temp.py
@@ -0,0 +1,8 @@
+#WAP to swap two variables using temp
+a=5
+b=4
+temp=a
+a=b
+b=temp
+print("After swp a =",a)
+print("After swp b =",b)
