d386fed4ced795095fbf3ce4d271890c4e99dbb6
Programwithurwashi <86796893+Programwithurwashi@users.noreply.github.com>
2022-03-12 02:36:20 -0800

    current working directory

---
+++ currentworkingdirectory.py
@@ -0,0 +1,11 @@
+#WAP to check the current working directory 
+import os
+
+def current_directory():
+    cwd=os.getcwd()
+    print(cwd)
+    
+current_directory()
+#def current_directory():
+   # cwd=os.get(cwd)
+    #print(cwd)
